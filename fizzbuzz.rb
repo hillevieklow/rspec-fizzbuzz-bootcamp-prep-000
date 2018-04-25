04ad949270c3e3922566d7d507201f54a5f93c41
Hillevi Eklöw <hillevieklow@gmail.com>
2018-04-25 17:14:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int % 5 == 0 and int % 3 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 5 == 0 and int % 3 == 0
-    "FizzBuzz"
   else
-    return nil
+    nil
   end
 end
